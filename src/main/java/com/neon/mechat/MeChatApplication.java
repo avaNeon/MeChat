98481dbe90729c6e7f9c9bb4c0e5674ac696441d
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-05-28 10:16:40 +0800

    feat(chore): 创建基本的环境配置文件

---
+++ src/main/java/com/neon/mechat/MeChatApplication.java
@@ -0,0 +1,15 @@
+package com.neon.mechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeChatApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MeChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neon/mechat/MeChatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.neon.mechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeChatApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
